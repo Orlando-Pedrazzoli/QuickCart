6449c46ef154b96ee82f10357a2aa3ae4ed98e64
Orlando Pedrazzoli <pedrazzoliorlando@gmail.com>
2025-03-13 22:11:20 +0000

    inngest updated

--- config/inngest.js
+++ config/inngest.js
@@ -7,10 +7,8 @@ export const inngest = new Inngest({ id: 'quickcart-next' });
 
 // Inngest function to save user data to a database
 export const syncUserCreation = inngest.createFunction(
-  {
-    id: 'sync-user-from-clerk',
-    event: 'clerk/user.created',
-  },
+  { id: 'sync-user-from-clerk' },
+  { event: 'clerk/user.created' },
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
